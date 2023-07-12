6861207c5c672024a3282a31235652b54e00b973
AlionaVr <alonavershilo@yandex.by>
2023-07-12 22:36:34 +0300

    Switch

--- src/Main.java
+++ src/Main.java
@@ -3,27 +3,27 @@ import java.util.Scanner;
 import java.util.stream.Stream;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String args[])
 
-        //переводит из галлов в литры//
-               /*System.out.println("Enter the volume in gallons: " );
-                Scanner consol = new Scanner(System.in);
-                double gallons = consol.nextDouble();
-                double liters = gallons*3.7854;
+        throws java.io.IOException {
+        char choice;
+        System.out.println("Справка: \n " +
+                            "1. if \n " +
+                            "2. switch \n " +
+                            "Выберите:");
+        choice = (char) System.in.read();
+        switch (choice) {
+            case '1':
+                System.out.println("Оператор if: \n if (условие) оператор;\n else оператор;");
+            break;
+            case '2':
+                System.out.println ("Оператор switch: \n switch(выражение){\n последовательность операторов\n" +
+                        "break;\n ... \n }");
+            break;
+            default:
+                System.out.print("Зaпpoc не найден.");
+        }
 
-                System.out.println("gallons = " + gallons);
-                System.out.print("liters = " + liters);*/
-
-        // *** задача поиска простых чисел *** //
-       int i,j;
-       boolean number;
-       System.out.println("простые числа:\n");
-       for (i=2; i<=100; i++) {
-           number = true;
-           for (j=2; j<=i/j; j++) {
-               if (i%j==0) number = false;}
-       if (number==true) System.out.println(i);
-       }
     }
 
 }
