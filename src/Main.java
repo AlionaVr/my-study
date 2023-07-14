6eab5853e83a8a9968cdab83c7008454915e141d
AlionaVr <alonavershilo@yandex.by>
2023-07-14 13:24:59 +0300

    Switch with while

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,22 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String args[])
-
         throws java.io.IOException {
-        char choice;
-        System.out.println("Справка: \n " +
-                            "1. if \n " +
-                            "2. switch \n " +
-                            "Выберите:");
-        choice = (char) System.in.read();
+        char choice, ignore;
+        //do {//
+            System.out.println("Cnpaвкa:");
+            System.out.println(" 1. if");
+            System.out.println(" 2. switch");
+            System.out.println(" З. for");
+            System.out.println(" 4. while");
+            System.out.println(" 5. do-while\n");
+            System.out.print("Bыбepитe: ");
+            choice = (char) System.in.read();
+            // Эта часть позволяет игнорировать все остальные цифры, повторяет справочное меню до тех пор пока не выберет нужный вариант//
+            /*do {
+                ignore = (char) System.in.read();}
+            while(ignore != '\n');}
+            while ( choice < '1' | choice > '5') ;*/
         switch (choice) {
             case '1':
                 System.out.println("Оператор if: \n if (условие) оператор;\n else оператор;");
@@ -20,6 +28,18 @@ public class Main {
                 System.out.println ("Оператор switch: \n switch(выражение){\n последовательность операторов\n" +
                         "break;\n ... \n }");
             break;
+            case '3':
+                System.out.println("Oпepaтop for:\n " +
+                        "for(инициaлизaция; условие; итерация)\n " +
+                        "оператор;");
+                break;
+            case '4':
+                System.out.println("Oпepaтop while:\n while(ycлoвиe) оператор;");
+                break;
+            case '5':
+                System.out.println("Oпepaтop do-while:\n" +
+                        "do {оператор;} while (условие);");
+                break;
             default:
                 System.out.print("Зaпpoc не найден.");
         }
