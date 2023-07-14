066a2fb9b8456171d2a6cc5de1e59092ddf5af24
AlionaVr <alonavershilo@yandex.by>
2023-07-14 14:52:05 +0300

    Switch with while

--- src/Main.java
+++ src/Main.java
@@ -6,20 +6,21 @@ public class Main {
     public static void main(String args[])
         throws java.io.IOException {
         char choice, ignore;
-        //do {//
-            System.out.println("Cnpaвкa:");
+        do {
+            System.out.println("Cпpaвкa:");
             System.out.println(" 1. if");
             System.out.println(" 2. switch");
-            System.out.println(" З. for");
+            System.out.println(" 3. for");
             System.out.println(" 4. while");
-            System.out.println(" 5. do-while\n");
-            System.out.print("Bыбepитe: ");
+            System.out.println(" 5. do-while");
+            System.out.println(" 6. break");
+            System.out.println(" 7. continue\n");
+            System.out.print("Bыбepитe (q - выход): ");
             choice = (char) System.in.read();
-            // Эта часть позволяет игнорировать все остальные цифры, повторяет справочное меню до тех пор пока не выберет нужный вариант//
-            /*do {
-                ignore = (char) System.in.read();}
-            while(ignore != '\n');}
-            while ( choice < '1' | choice > '5') ;*/
+            do {
+                ignore = (char) System.in.read();
+            } while (ignore != '\n');
+        }while( choice < '1' | choice > '7' & choice != 'q');
         switch (choice) {
             case '1':
                 System.out.println("Оператор if: \n if (условие) оператор;\n else оператор;");
@@ -39,6 +40,14 @@ public class Main {
             case '5':
                 System.out.println("Oпepaтop do-while:\n" +
                         "do {оператор;} while (условие);");
+                break;
+            case '6':
+                System.out.println("Oпepaтop break:\n"+
+                        "break; или break метка;");
+                break;
+            case '7':
+                System.out.println("Oпepaтop continue:\n"+
+                        "continue; или continue метка;");
                 break;
             default:
                 System.out.print("Зaпpoc не найден.");
