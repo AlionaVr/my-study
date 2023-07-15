ebb18f7923e592ca9d903569820d60f994d4678d
AlionaVr <alonavershilo@yandex.by>
2023-07-15 16:15:12 +0300

    Counter of Space

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,22 @@
-import java.io.IOException;
-import java.util.Scanner;
-import java.util.stream.Stream;
-
 public class Main {
     public static void main(String args[])
             throws java.io.IOException {
+        // меняет регистр введенных символов//
         char symbol;
-        int counter=0;
-        System.out.println("для остановки введите '.' ");
+        int changes = 0;
+        System.out.println("Введите символы. Для остановки введите '.'");
         do {
             symbol = (char) System.in.read();
-            if (symbol == ' ') counter++;
+            if (symbol >= 'a' & symbol <= 'z') {
+                symbol -= 32;
+                changes++;
+            } else if (symbol >= 'A' & symbol <= 'Z') {
+                symbol += 32;
+                changes++;
+            }
         }
-        while (symbol != '.');
-        System.out.println("counter:"+ counter);
-        System.out.println("конец");
+        while (symbol != '\n');
+        System.out.println("Количество изменений регистра:" + changes);
     }
 }
 
