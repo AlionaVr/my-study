c4defa02456e3884200f00333b7b43de1ae4ffcb
AlionaVr <alonavershilo@yandex.by>
2023-11-02 13:50:23 +0300

    Odd or Even

---
+++ src/WriterReader.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+public class WriterReader {
+    public static void main(String[] args) throws FileNotFoundException {
+        PrintWriter writer = new PrintWriter("text");
+        writer.println("OLEG");
+        writer.close();
+
+        try (FileWriter fileWriter = new FileWriter("text", true)){
+            fileWriter.append("OlEG2");
+        } catch (Exception ignored) {}
+
+    }
+}
