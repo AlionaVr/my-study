3da11e1c83ea4ee9ed905f289bca16ae7c263b09
AlionaVr <alonavershilo@yandex.by>
2023-11-02 17:23:39 +0300

    Leap years?(rewrited)

--- src/LeapYears.java
+++ src/LeapYears.java
@@ -1,9 +1,6 @@
 public class  LeapYears {
     public static boolean isLeapYear(int year) {
-        if (year % 100 == 0&&year%400!=0) {
-            return false;}
-            return year % 4== 0||year%400==0;
-
+       return year % 100 != 0&&year % 4== 0||year%400==0;
         }
     public static void main(String[] args) {
         boolean leapYear = isLeapYear(2100);
